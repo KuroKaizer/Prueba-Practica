265a741a1b4cf55d8d005f76261493fc98a5ff6f
KuroKaizer <manueludouni@gmail.com>
2023-02-01 01:22:35 -0400

    Creación de los endpoints
    Creación de todas las rutas necesarias para los endpoints

--- routes/estudiantes.py
+++ routes/estudiantes.py
@@ -3,6 +3,32 @@ from fastapi import APIRouter
 
 estudiante = APIRouter()
 
-@estudiante.get("/estudiantes")
-def helloworld():
+#Enviar solicitud de ingreso
+@estudiante.post("/estudiantes")
+def enviar_solicitud():
     return "hello world"
+
+#Actualizar solicitud de ingreso
+@estudiante.put("/estudiantes")
+def actualizar_solicitud():
+    return "hello world"
+
+#Actaulizar estatus de la solicitud
+@estudiante.put("/estudiantes")
+def actualizar_estatus():
+    return "hello world"
+
+#Consultar todas las solicitudes
+@estudiante.get("/estudiantes")
+def consultar_todo():
+    return "hello world"
+
+#Consultar asignaciones de Grimorios
+@estudiante.get("/estudiantes")
+def consultar_grimorio():
+    return "hello world"
+
+#Eliminar solicitud de ingreso
+@estudiante.delete("/estudiantes")
+def eliminar_solicitud():
+    return "hello world"                    
